009d4b09ae59408ca5b46c7392166870e3d1f9a8
John Tongelidis <tongelja@hotmail.com>
2020-10-10 22:30:26 -0500

    Updated for new api version.  Added sound and synonyms/antonyms

--- words.py
+++ words.py
@@ -1,5 +1,10 @@
 import requests, json, random, argparse, logging, time, random
+import contextlib
+import os
 
+with open(os.devnull, 'w') as null,  contextlib.redirect_stdout(null), contextlib.redirect_stderr(null):
+    from pygame import mixer
+    
 class color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -8,10 +13,11 @@ class color:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
-    #BOLD = '\033[1m'
-    BOLD = '**'
+    BOLD = '\033[1m'
+    ##BOLD = '**'
     UNDERLINE = '\033[4m'
-    END = '**'
+    END = '\033[0m'
+    ##END = '**'
 
 
 class WordList():
@@ -48,60 +54,20 @@ class WordList():
         print(self.word_raw)
         #print(self.sentences_raw)
  
-    def getDefinition(self):
-
-        word_id = self.word_id 
-        app_id = self.app_id
-        app_key = self.app_key
-        language = self.language
-        s = []
-
-        url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.strip().lower()
-
-        r = requests.get( url, headers = {'app_id': app_id, 'app_key': app_key} )
-
-        word_json = json.loads( json.dumps(r.json()) )
-
-        self.word_raw = r.text
-
-        word = []
-
-        for results in word_json['results']:
-            for lexicalEntries in results['lexicalEntries']:
-                if lexicalEntries['lexicalCategory'] is not None:
-                    word.append(color.BOLD + 'Category: ' + color.END + lexicalEntries['lexicalCategory'])
-
-                if 'pronunciations' in lexicalEntries:
-                    for pronunciations in lexicalEntries['pronunciations']: 
-                        if 'phoneticSpelling' in pronunciations:
-                            word.append(color.BOLD + 'Phonetic Spelling: ' + color.END + pronunciations['phoneticSpelling'] )
-
-                if 'entries' in lexicalEntries:
-                    for entries in lexicalEntries['entries']:
-                        if 'etymologies' in entries:
-                            for l in entries['etymologies']:
-                                word.append(color.BOLD + 'Etymology: ' + color.END  + l)
-                        if 'senses' in entries:
-                            for senses in entries['senses']:
-                                if 'definitions' in senses:
-                                    for definitions in senses['definitions']:
-                                        word.append(color.BOLD + 'Definition: ' + color.END  + definitions)
-
-        return word
-
     def lookUpWord(self):
         
         self.definition_list  = self.getDefinition()
+        self.thesaurus_list   = self.getThesaurus()
         self.sentence_list    = self.getSentence()
        
     def printWord(self):
 
         word_id = self.word_id
 
-        print('\n\n')
-        print('[verse]')
-        print(color.BOLD + 'Word: ' + color.END + word_id.replace('_', ' ') )
+        print('\n')
+        print(color.BOLD + color.UNDERLINE + 'Word: ' + word_id.replace('_', ' ') + color.END )
         self.printDefinition()
+        self.printThesaurus()
         self.printSentence()
         print()
 
@@ -114,6 +80,59 @@ class WordList():
         for i in self.sentence_list:
             print(i)
 
+    def printThesaurus(self):
+ 
+        for i in self.thesaurus_list:
+            print(i)
+
+    def getDefinition(self):
+
+        word_id = self.word_id 
+        app_id = self.app_id
+        app_key = self.app_key
+        language = self.language
+        s = []
+
+        url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.strip().lower()
+
+        r = requests.get( url, headers = {'app_id': app_id, 'app_key': app_key} )
+
+        word_json = json.loads( json.dumps(r.json()) )
+
+        self.word_raw = r.text
+
+        word = []
+
+        for results in word_json['results']:
+            for lexicalEntry in results['lexicalEntries']:
+                if lexicalEntry['lexicalCategory'] is not None:
+                    word.append(color.BLUE + 'Category: ' + color.END + lexicalEntry['lexicalCategory']['text'])
+
+                if 'entries' in lexicalEntry:
+                    for entry in lexicalEntry['entries']:
+                        for pronunciation in entry['pronunciations']:
+                            if 'audioFile' in pronunciation:
+                                mp3 = requests.get(pronunciation['audioFile'])
+                                tmp_mp3 = '/Users/tongeljl/Downloads/wod.mp3'
+                                with open(tmp_mp3, 'wb') as f:
+                                    f.write(mp3.content)
+
+                                mixer.init()
+                                mixer.music.load(tmp_mp3)
+                                mixer.music.play()
+
+                            word.append(color.YELLOW + 'Phonetic Spelling: ' + color.END  + pronunciation['phoneticSpelling'])
+                        if 'etymologies' in entry:
+                            for l in entry['etymologies']:
+                                word.append(color.YELLOW + 'Etymology: ' + color.END  + l)
+                        if 'senses' in entry:
+                            for senses in entry['senses']:
+                                if 'definitions' in senses:
+                                    for definitions in senses['definitions']:
+                                        word.append(color.RED + 'Definition: ' + color.END  + definitions)
+
+        return word
+
 
     def getSentence(self):
 
@@ -124,7 +143,7 @@ class WordList():
         s = []
 
 
-        url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.strip().lower() + '/sentences'
+        url = 'https://od-api.oxforddictionaries.com:443/api/v2/sentences/' + language + '/' + word_id.strip().lower() 
 
         r = requests.get( url, headers = {'app_id': app_id, 'app_key': app_key} )
 
@@ -132,17 +151,63 @@ class WordList():
             word_json = json.loads( json.dumps(r.json()) ) 
 
             self.sentences_raw = r.text
+            if 'results' in word_json:
+                for results in word_json['results']:
+                    for lexicalEntry in results['lexicalEntries']:
+                        if len(lexicalEntry['sentences']) > 5:
+                            sentence_len=5
+                        else:
+                            sentence_len=len(lexicalEntry['sentences'])
 
-            for results in word_json['results']:
-                for lexicalEntries in results['lexicalEntries']:
-                    if len(lexicalEntries['sentences']) > 5:
-                        sentence_len=5
-                    else:
-                        sentence_len=len(lexicalEntries['sentences'])
+                        for sentence in random.sample(lexicalEntry['sentences'], sentence_len):
+                            s.append( (color.CYAN + 'Sentence: ' + color.END + sentence['text']) )
 
-                    for sentences in random.sample(lexicalEntries['sentences'], sentence_len):
-                        s.append( (color.BOLD + 'Sentence: ' + color.END + sentences['text']) )
+        except json.decoder.JSONDecodeError:
+            pass
+ 
+        return s
 
+
+    def getThesaurus(self):
+
+        word_id = self.word_id
+        app_id = self.app_id
+        app_key = self.app_key
+        language = self.language
+        s = []
+
+
+        url = 'https://od-api.oxforddictionaries.com:443/api/v2/thesaurus/' + language + '/' + word_id.strip().lower() 
+
+        r = requests.get( url, headers = {'app_id': app_id, 'app_key': app_key} )
+
+        try: 
+            word_json = json.loads( json.dumps(r.json()) ) 
+            self.antonyms_raw = r.text
+          
+            if 'results' in word_json:
+                for results in word_json['results']:
+                    for lexicalEntry in results['lexicalEntries']:
+                        for entry in lexicalEntry['entries']:
+                            ##sense = entry['senses'][0]
+                            for sense in entry['senses']:
+                                if 'antonyms' in sense:
+                                    if len(sense['antonyms']) > 3:
+                                        antonym_len=3
+                                    else:
+                                        antonym_len=len(sense['antonyms'])
+
+                                    for antonym in random.sample(sense['antonyms'], antonym_len):
+                                        s.append( (color.DARKCYAN + 'Antonym: ' + color.END + antonym['text']) )
+
+                                if 'synonyms' in sense:
+                                    if len(sense['synonyms']) > 3:
+                                        synonym_len=3
+                                    else:
+                                        synonym_len=len(sense['synonyms'])
+
+                                    for synonym in random.sample(sense['synonyms'], synonym_len):
+                                        s.append( (color.PURPLE + 'Synonym: ' + color.END + synonym['text']) )
         except json.decoder.JSONDecodeError:
             pass
  
