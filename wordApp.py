8ed5b08ae80b27c5f7d9fd4924f43566d19eab8e
John Tongelidis <tongelja@hotmail.com>
2021-06-11 11:11:08 -0500

    Add search for word functionality

--- wordApp.py
+++ wordApp.py
@@ -5,9 +5,17 @@ import words
 app = Flask(__name__)
 
 
-@app.route('/word/')
+@app.route('/')
 def word():
     wordlist = words.WordList(web=True)
+    definition, audio  = wordlist.getDefinition()
+    thesaurus = wordlist.getThesaurus()
+    sentences = wordlist.getSentence()
+    return render_template('word.html', word=wordlist.word_id, define=definition, thes=thesaurus, sent=sentences, aud=audio)
+
+@app.route('/<myword>')
+def user_word(myword):
+    wordlist = words.WordList(myword, True)
     definition, audio  = wordlist.getDefinition()
     thesaurus = wordlist.getThesaurus()
     sentences = wordlist.getSentence()
